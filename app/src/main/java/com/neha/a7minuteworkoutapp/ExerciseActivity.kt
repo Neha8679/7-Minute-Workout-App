dba598c735cbb132af88c7d29603ba27a6d36d68
Neha8679 <safercode067@gmail.com>
2025-06-17 01:00:59 +0530

    using countdown timers to display the second remaining

--- app/src/main/java/com/neha/a7minuteworkoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/neha/a7minuteworkoutapp/ExerciseActivity.kt
@@ -2,10 +2,14 @@ package com.neha.a7minuteworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.neha.a7minuteworkoutapp.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private var binding : ActivityExerciseBinding ? = null
+    private var restTimer : CountDownTimer? =null
+    private var restProgress =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -19,6 +23,44 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener{
             onBackPressed()
         }
+        setUpRestView()
 
+    }
+
+    private fun setUpRestView(){
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress=0
+        }
+
+        setRestProgressBar()
+
+    }
+    private fun setRestProgressBar(){
+        binding?.ProgressBar?.progress = restProgress
+         restTimer = object: CountDownTimer(10000, 1000){
+             override fun onTick(millisUntilFinished: Long) {
+                 restProgress++
+                 binding?.ProgressBar?.progress = 10 - restProgress
+                 binding?.tvTimer?.text = (10 - restProgress).toString()
+             }
+
+             override fun onFinish() {
+                 Toast.makeText(
+                     this@ExerciseActivity,"Here now we will start the exercise.",Toast.LENGTH_SHORT
+                 ).show()
+             }
+
+         }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress=0
+        }
+        binding=null
     }
 }
--- app/src/main/java/com/neha/a7minuteworkoutapp/MainActivity.kt
+++ app/src/main/java/com/neha/a7minuteworkoutapp/MainActivity.kt
@@ -1,32 +1,81 @@
 package com.neha.a7minuteworkoutapp
 
 import android.content.Intent
+import android.content.IntentSender.OnFinished
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.neha.a7minuteworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var binding : ActivityMainBinding?= null
+  private var binding : ActivityMainBinding?= null
+//    private var countDownTimer: CountDownTimer? = null
+//    private var timerDuration: Long = 60000
+//    private var pauseOffset: Long = 0
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+       binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
         setContentView(binding?.root)
+//        setSupportActionBar(toolbar )
+//        tvTimer.text = "${(timerDuration/1000).toString()}"
 
-        //val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        val flStartButton : FrameLayout = findViewById(R.id.flStart)
+//        btnStart.setOnClickListener{
+//            startTimer(pauseOffset)
+//        }
         binding?.flStart?.setOnClickListener {
             val intent =Intent(this,ExerciseActivity::class.java)
             startActivity(intent)
 
         }
+//        btnPause.setOnClickListener{
+//            pauseTimer()
+//        }
+//
+//        btnStop.setOnClickListener{
+//            resetTimer()
+//        }
     }
+
+//    private fun startTimer(pauseOffsetL: Long){
+//        countDownTimer = object : CountDownTimer(timerDuration - pauseOffsetL,1000){
+//            override fun onTick(millisUnitFinished: Long) {
+//                pauseOffset=timerDuration - millisUnitFinished
+//                tvTimer.text = (millisUnitFinished/1000).toString()
+//            }
+//
+//            override fun onFinish() {
+//                Toast.makeText(this@MainActivity,"Timer is finished.",Toast.LENGTH_SHORT).show()
+//            }
+//        }.start()
+//    }
+
+//    private fun pauseTimer(){
+//        if(countDownTimer!=null){
+//            countDownTimer!!.cancel()
+//        }
+//    }
+
+
+//    private fun resetTimer(){
+//        if(countDownTimer !=null){
+//            countDownTimer!!.cancel()
+//            tvTimer.text="${(timerDuration/1000).toString()}"
+//            countDownTimer = null
+//            pauseOffset =0
+//        }
+//    }
 
     override fun onDestroy() {
         super.onDestroy()
         binding = null
     }
 }
+
