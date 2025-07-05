36297e80a1b1922d51aeeeeaa0f671569a7681b4
Neha8679 <safercode067@gmail.com>
2025-07-05 19:50:15 +0530

    displaying at which exercise we currently are

--- app/src/main/java/com/neha/a7minuteworkoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/neha/a7minuteworkoutapp/ExerciseActivity.kt
@@ -123,6 +123,11 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
              override fun onFinish() {
                  currentExercisePosition++
+
+                 exerciseList!![currentExercisePosition].setIsSelected(true)
+                 exerciseAdapter!!.notifyDataSetChanged()
+
+
                  setUpExerciseView()
              }
 
@@ -140,6 +145,13 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             }
 
             override fun onFinish() {
+
+                exerciseList!![currentExercisePosition].setIsSelected(false)
+                exerciseList!![currentExercisePosition].setIsCompleted(true)
+                exerciseAdapter!!.notifyDataSetChanged()
+
+
+
                 if(currentExercisePosition<exerciseList?.size!!-1){
                     setUpRestView()
                 }else{
